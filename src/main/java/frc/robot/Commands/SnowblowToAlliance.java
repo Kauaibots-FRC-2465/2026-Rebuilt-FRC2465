d8e513875c5ad8b5f302f420bfc4ab8402f7a270
Chiem Ma <chiemwhuama@gmail.com>
2026-03-29 15:05:47 -1000

    Targeting works correclty

--- src/main/java/frc/robot/Commands/SnowblowToAlliance.java
+++ src/main/java/frc/robot/Commands/SnowblowToAlliance.java
@@ -1,7 +1,5 @@
 package frc.robot.Commands;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Objects;
 import java.util.function.Supplier;
 
@@ -16,7 +14,6 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StringPublisher;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.fieldmath.FieldMath;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
@@ -31,25 +28,12 @@ import frc.robot.subsystems.SparkAnglePositionSubsystem;
  * of the drivetrain, horizontal aim, vertical aim, and shooter subsystems.
  */
 public class SnowblowToAlliance extends Command {
-    private static final long DRIVE_DIRECTION_LOOKBACK_MICROSECONDS = 250_000;
-
-    private static class PoseSample {
-        final Translation2d translation;
-        final long timestampMicroseconds;
-
-        PoseSample(Translation2d translation, long timestampMicroseconds) {
-            this.translation = translation;
-            this.timestampMicroseconds = timestampMicroseconds;
-        }
-    }
-
     private final CommandSwerveDrivetrain drivetrain;
     private final Supplier<SwerveRequest> driveRequestSupplier;
     private final SwerveRequest.FieldCentricFacingAngle facingAngleDrive =
             new SwerveRequest.FieldCentricFacingAngle();
     private final DoubleArrayPublisher targetPublisher;
     private final StringPublisher fieldTypePublisher;
-    private final Deque<PoseSample> recentPoseSamples = new ArrayDeque<>();
     private Translation2d lastFieldRelativeDriveDirection = new Translation2d();
 
     public SnowblowToAlliance(
@@ -69,7 +53,7 @@ public class SnowblowToAlliance extends Command {
         fieldTypePublisher = snowblowTable.getStringTopic(".type").publish();
         fieldTypePublisher.set("Field2d");
 
-        facingAngleDrive.withHeadingPID(4.0, 0.0, 0.0);
+        facingAngleDrive.withHeadingPID(5.0, 0.0, 0.0);
         facingAngleDrive.HeadingController.enableContinuousInput(-Math.PI, Math.PI);
 
         addRequirements(this.drivetrain, horizontalAim, verticalAim, shooter);
@@ -77,19 +61,17 @@ public class SnowblowToAlliance extends Command {
 
     @Override
     public void initialize() {
-        recentPoseSamples.clear();
-        recordCurrentPoseSample();
     }
 
     @Override
     public void execute() {
         SwerveRequest requestedDrive = driveRequestSupplier.get();
-        updateLastDriveDirection();
         if (!(requestedDrive instanceof SwerveRequest.FieldCentric fieldCentricRequest)) {
             drivetrain.setControl(requestedDrive);
             return;
         }
 
+        updateLastDriveDirection(fieldCentricRequest);
         Translation2d target = getTarget();
         targetPublisher.set(new double[] {target.getX(), target.getY(), 0.0});
         Rotation2d targetDirection = target.minus(drivetrain.getState().Pose.getTranslation()).getAngle();
@@ -115,7 +97,6 @@ public class SnowblowToAlliance extends Command {
 
     @Override
     public void end(boolean interrupted) {
-        recentPoseSamples.clear();
     }
 
     /**
@@ -127,41 +108,9 @@ public class SnowblowToAlliance extends Command {
         return FieldMath.getSnowblowTarget(robotPose, lastFieldRelativeDriveDirection, alliance);
     }
 
-    private void updateLastDriveDirection() {
-        recordCurrentPoseSample();
-        PoseSample currentSample = recentPoseSamples.peekLast();
-        PoseSample referenceSample = getReferencePoseSample(currentSample.timestampMicroseconds);
-        if (currentSample != null && referenceSample != null) {
-            lastFieldRelativeDriveDirection = FieldMath.updateLastDriveDirection(
-                    currentSample.translation,
-                    referenceSample.translation,
-                    lastFieldRelativeDriveDirection);
-        }
-    }
-
-    private void recordCurrentPoseSample() {
-        recentPoseSamples.addLast(new PoseSample(
-                drivetrain.getState().Pose.getTranslation(),
-                RobotController.getFPGATime()));
-    }
-
-    private PoseSample getReferencePoseSample(long currentTimestampMicroseconds) {
-        long cutoffTimestampMicroseconds = currentTimestampMicroseconds - DRIVE_DIRECTION_LOOKBACK_MICROSECONDS;
-        while (recentPoseSamples.size() > 1) {
-            java.util.Iterator<PoseSample> poseIterator = recentPoseSamples.iterator();
-            poseIterator.next();
-            PoseSample nextOldestSample = poseIterator.next();
-            if (nextOldestSample.timestampMicroseconds <= cutoffTimestampMicroseconds) {
-                recentPoseSamples.removeFirst();
-                continue;
-            }
-            break;
-        }
-
-        PoseSample oldestSample = recentPoseSamples.peekFirst();
-        if (oldestSample != null && oldestSample.timestampMicroseconds <= cutoffTimestampMicroseconds) {
-            return oldestSample;
-        }
-        return null;
+    private void updateLastDriveDirection(SwerveRequest.FieldCentric fieldCentricRequest) {
+        lastFieldRelativeDriveDirection = FieldMath.updateLastDriveDirection(
+                new Translation2d(fieldCentricRequest.VelocityX, fieldCentricRequest.VelocityY),
+                lastFieldRelativeDriveDirection);
     }
 }
